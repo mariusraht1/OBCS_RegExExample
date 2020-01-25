c85cee6bdcbe6fff143a54c228be6f2c43e83d3c
mariusraht2 <mariusraht+2@gmail.com>
2020-01-25 17:44:24 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using ParserLibrary;
+using System;
+using System.Collections;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = "input.txt";
+            string outputFile = "output.txt";
+
+            //ToDo: Einlesen der Datei sowie Aufruf der Konvertierungs-Methode und Ausgabe der Ergebnisse
+            ArrayList input = new ArrayList();
+
+            using (StreamReader reader = new StreamReader(inputFile))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    input.Add(line);
+                }
+            }
+
+            string[] output = AddressParser.convertFileContent(input);
+
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                for (int i = 0; i < output.Length; i++)
+                {
+                    writer.WriteLine(output[i]);
+                }
+            }
+
+            Console.WriteLine("Done.");
+            Console.Read();
+        }
+    }
+}
---
+++ ParserLibrary/AddressParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ParserLibrary
+{
+    public class AddressParser
+    {
+        public static string[] convertFileContent(ArrayList input)
+        {
+            string[] result = new string[input.Count];
+
+            // ToDo: Konvertierung der eingehenden ArrayList in das gewünschte Ausgabeformat mit Hilfe regulärer Ausdrücke
+            //    Vorname Nachname|Straße Hausnr.|PLZ|Ort|Land
+            // => Nachname, Vorname, Straße, Hausnr., Land, PLZ, Ort
+            for (int i = 0; i < input.Count; i++)
+            {
+                string[] entry = Regex.Split(input[i].ToString(), "^(.*)\\s(.*)\\|(.*)\\s(.*)\\|(.*)\\|(.*)\\|(.*)$");
+                entry = entry.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+                result[i] = $"{entry[1]}, {entry[0]}, {entry[2]}, {entry[3]}, {entry[6]}, {entry[4]}, {entry[5]}";
+            }
+            
+            return result;
+        }
+    }
+}
